10929b06d3791127ed0192350d86dd28e0ffd833
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-14 15:49:23 -0600

    New Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ featured = [true, false]
     author: Faker::App.author,
     category: categories.sample,
     price: Faker::Commerce.price.to_f,
-    logo: Faker::Company.logo,
+    logo: Faker::Avatar.image("my-own-slug", "50x50", "bmp", "set2", "bg1"),
     featured: featured.sample
   )
 end
